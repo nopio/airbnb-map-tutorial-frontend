a1286b8a7871e589535b3df1c641559a874cd7d5
Piotr Jaworski <piotrek.jaw@gmail.com>
2017-09-24 18:38:02 +0200

    Basic google map

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,10 +1,42 @@
 import React, { Component } from 'react'
+import { withGoogleMap, GoogleMap } from 'react-google-maps'
+
+const AirbnbMap = withGoogleMap(props => (
+  <GoogleMap
+    defaultCenter={props.center}
+    defaultZoom={props.zoom} />
+));
 
 export class Map extends Component {
+  constructor(props) {
+    super(props)
+
+    this.zoom = 7
+
+    this.state = {
+      lat: 50.0515918,
+      lng: 19.9357531
+    };
+  }
+
   render() {
+    var {lat, lng} = this.state;
+
     return(
-      <div>
-        Map
+      <div style={{width: `750px`, height: `750px`}}>
+        <AirbnbMap
+          center={{
+            lat: lat,
+            lng: lng
+          }}
+          zoom={this.zoom}
+          containerElement={
+            <div style={{ height: `100%` }} />
+          }
+          mapElement={
+            <div style={{ height: `100%` }} />
+          }
+        />
       </div>
     );
   }
